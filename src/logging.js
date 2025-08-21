cb52a7b299c15971d31aa83432fec4adf8218000
cmprog <2270827+cmprog@users.noreply.github.com>
2025-08-20 20:10:10 -0500

    removing UI logging from non-localhost again

--- src/logging.js
+++ src/logging.js
@@ -39,7 +39,7 @@ class Logger {
         this.scrollElement = document.querySelector('.log-container > .content');
         
         this.consoleLoggingEnabled = false;
-        this.uiLoggingEnabled = true || (document.location.href.indexOf('localhost') >= 0);
+        this.uiLoggingEnabled = (document.location.href.indexOf('localhost') >= 0);
         
         if (!this.uiLoggingEnabled) {
             document.querySelector('.log-container').classList.add('hidden');
