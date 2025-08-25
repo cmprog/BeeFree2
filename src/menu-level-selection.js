22b660b7f45a20dd49bc0b1295d1f39d56008e63
cmprog <2270827+cmprog@users.noreply.github.com>
2025-08-24 19:08:14 -0500

    levels now lock in the menu

--- src/menu-level-selection.js
+++ src/menu-level-selection.js
@@ -6,13 +6,16 @@ import { registerClick } from './util.js';
 
 class LevelSelectionItem {
 
-    constructor(levelDefinition) {
+    constructor(parent, levelDefinition) {
 
+        this.parent = parent;
         this.definition = levelDefinition;
+        this.isUnlocked = false;
 
         const templateHtml = `
             <button type="button" class="level">
                 <div class="level-title">${levelDefinition.name}</div>
+                <div class="level-locked-status">(locked)</div>
                 <div class="level-badge-container">
                     <div class="level-badge level-badge-flawless">F</div>
                     <div class="level-badge level-badge-perfect">P</div>
@@ -26,14 +29,28 @@ class LevelSelectionItem {
         this.flawlessBadgeElement = this.listItemEl.querySelector('.level-badge-flawless');
         this.perfectBadgeElement = this.listItemEl.querySelector('.level-badge-perfect');
         this.element = this.listItemEl.querySelector('button');
+       
+        registerClick(this.element, this.loadLevel.bind(this));
         
-        this.refreshBadges();
+        this.reload();
     }
 
-    refreshBadges() {
+    reload() {
+
         const levelData = currentPlayer.getLevel(this.definition.id);
+
         this.updateBadgeClass(this.flawlessBadgeElement, levelData.flawlessCount > 0);
         this.updateBadgeClass(this.perfectBadgeElement, levelData.perfectCount > 0);
+
+        const CLASS_NAME_UNLOCKED = 'unlocked';
+
+        this.isUnlocked = levelData.isUnlocked;
+
+        if (this.isUnlocked) {
+            this.element.classList.add(CLASS_NAME_UNLOCKED);
+        } else {
+            this.element.classList.remove(CLASS_NAME_UNLOCKED);
+        }
     }
 
     updateBadgeClass(element, hasBadge) {
@@ -42,6 +59,14 @@ class LevelSelectionItem {
             element.classList.add(CLASS_NAME);
         } else {
             element.classList.remove(CLASS_NAME);
+        }
+    }
+
+    loadLevel() {
+
+        if (this.isUnlocked) {
+            this.parent.close();
+            this.definition.startLevel();
         }
     }
 }
@@ -56,10 +81,8 @@ export class LevelSelectionMenu extends Menu {
 
         for (const levelDefinition of LEVELS) {
 
-            const button = new LevelSelectionItem(levelDefinition);
+            const button = new LevelSelectionItem(this, levelDefinition);
             this.buttons.push(button);
-
-            registerClick(button.element, this.loadLevel.bind(this, levelDefinition));
             
             const itemElement = document.createElement('li');
             itemElement.appendChild(button.element);
@@ -70,12 +93,7 @@ export class LevelSelectionMenu extends Menu {
 
     onOpening() {
         for (const button of this.buttons) {
-            button.refreshBadges();
+            button.reload();
         }
-    }
-
-    loadLevel(levelDefinition) {        
-        this.close();
-        levelDefinition.startLevel();
     }
 }
